45757f1a3ba82f1a4feb11792c5b086e52b60c36
polyakhtar <poly09876pm@gmail.com>
2022-11-28 09:34:52 +0600

    home page route setup

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import SellUpWorkProcess from '../SellUpWorkProcess/SellUpWorkProcess';
 import Categories from './Categories/Categories';
-
 const Home = () => {
     return (
         <div className=''>
