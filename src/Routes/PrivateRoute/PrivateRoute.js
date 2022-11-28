1ea3f552a6681f3b7855861e462a3177a415a858
polyakhtar <poly09876pm@gmail.com>
2022-11-28 09:42:08 +0600

    private route set up

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
-
 const PrivateRoute = ({children}) => {
 const {user,loading}=useContext(AuthContext);
 const location=useLocation();
